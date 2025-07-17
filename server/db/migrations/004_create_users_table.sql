6ed366acdde8f9fe1835ae4c69f9522dc718ac3a
tobimadehin <tobimadehin@gmail.com>
2025-07-17 22:17:19 +0100

    Added avatar to users table

--- server/db/migrations/004_create_users_table.sql
+++ server/db/migrations/004_create_users_table.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     name           TEXT      NOT NULL,
     email          TEXT      NOT NULL UNIQUE,
     role           TEXT      NOT NULL,
+    avatar         TEXT,
     created_at     DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at     DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
