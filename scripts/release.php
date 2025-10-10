c429860b2c6651040c597980c88a4549e40399b1
tobimadehin <tobimadehin@gmail.com>
2025-10-10 02:08:52 +0100

    Bug fix - push files before tag

--- scripts/release.php
+++ scripts/release.php
@@ -88,5 +88,6 @@ echo "Bumped version from $currentVersion to $newVersion\n";
 exec('git tag ' . escapeshellarg("v$newVersion"));
 exec('git add -A');
 exec('git commit -m ' . escapeshellarg($options['message']));
+exec('git push');
 exec('git push origin ' . escapeshellarg("v$newVersion"));
 echo "Successfully released version $newVersion\n";
