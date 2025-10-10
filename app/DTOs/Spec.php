ec33770f9288a941d719340c55339edc98b4685f
tobimadehin <tobimadehin@gmail.com>
2025-10-10 13:17:32 +0100

    DTO for blueprint config

---
+++ app/DTOs/Spec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Models\Remote;
+
+readonly class Spec
+{
+    public function __construct(
+        public string $id,
+        public string $serviceName,
+        public ?Remote $remote,
+        public ?string $commitHash,
+        public ?Remote $ciRemote,
+        public string $runtime,
+        public ?int $port,
+        public ?string $image,
+        public array $envVars = [],
+        public array $secrets = [],
+        public ?string $outputDir,
+        public ?string $workingDir,
+        public ?string $runCmd,
+    ) {}
+}
