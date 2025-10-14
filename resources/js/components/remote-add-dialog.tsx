7e13b46b4b7ef18172666c3a212ec6588fa7f6c8
tobimadehin <tobimadehin@gmail.com>
2025-10-14 10:18:07 +0100

    Tiny text update

--- resources/js/components/remote-add-dialog.tsx
+++ resources/js/components/remote-add-dialog.tsx
@@ -63,7 +63,7 @@ export default function RemoteAddDialog({ open, setOpen }: Props) {
                                     <div className="flex justify-center">
                                         <div className="flex items-center gap-2 text-sm text-muted-foreground">
                                             <Loader2 className="h-4 w-4 animate-spin" />
-                                            {searchComplete ? 'Creating project...' : 'Fetching remote repository...'}
+                                            {searchComplete ? 'Creating project' : 'Fetching remote repository'}
                                         </div>
                                     </div>
                                 )}
