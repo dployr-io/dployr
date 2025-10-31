fcd7ec5221418420837d73d0ba76d9edde559134
tobimadehin <tobimadehin@gmail.com>
2025-10-31 04:21:17 +0100

    updated initializer

--- internal/service/service.go
+++ internal/service/service.go
@@ -13,18 +13,30 @@ type Servicer struct {
 	store  store.ServiceStore
 }
 
-func (s Servicer) CreateService(ctx context.Context, svc *store.Service) (*store.Service, error) {
-	return s.store.CreateService(ctx, svc)
+func Init(cfg *shared.Config, logger *slog.Logger, store store.ServiceStore) *Servicer {
+	return &Servicer{
+		cfg:    cfg,
+		logger: logger,
+		store:  store,
+	}
 }
 
-func (s Servicer) GetService(ctx context.Context, id string) (*store.Service, error) {
+func (s *Servicer) CreateService(ctx context.Context, svc *store.Service) (store.Service, error) {
+	result, err := s.store.CreateService(ctx, svc)
+	if err != nil {
+		return store.Service{}, err
+	}
+	return *result, nil
+}
+
+func (s *Servicer) GetService(ctx context.Context, id string) (*store.Service, error) {
 	return s.store.GetService(ctx, id)
 }
 
-func (s Servicer) ListServices(ctx context.Context, limit, offset int) ([]*store.Service, error) {
+func (s *Servicer) ListServices(ctx context.Context, userID string, limit, offset int) ([]*store.Service, error) {
 	return s.store.ListServices(ctx, limit, offset)
 }
 
-func (s Servicer) UpdateService(ctx context.Context, svc *store.Service) error {
-	return s.store.UpdateService(ctx, svc)
+func (s *Servicer) UpdateService(ctx context.Context, id string, svc store.Service) error {
+	return s.store.UpdateService(ctx, &svc)
 }
